870ec92326cfb75fcef829ce06c44d6bbc75ba07
David Souza <dabcs@poli.ufrj.br>
2022-05-16 19:47:10 -0300

    style: fix checkstyle

--- src/main/java/com/trybe/acc/java/controledeacesso/HandleGuest.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/HandleGuest.java
@@ -1,9 +1,11 @@
 package com.trybe.acc.java.controledeacesso;
 
-import java.text.NumberFormat;
 import java.util.ArrayList;
 
 public class HandleGuest {
+  /**
+   * Handle ranges of ages.
+   */
   public void checkAge(short age) {
     if (age < 18) {
       System.out.println("Pessoa cliente menor de idade, catraca liberada!");
@@ -14,8 +16,14 @@ public class HandleGuest {
     }
   }
 
+  /**
+   *Generate access report.
+   */
   public void generateReport(ArrayList<Short> ages) {
-    int minor = 0, major = 0, senior = 0;
+    int minor = 0;
+    int major = 0;
+    int senior = 0;
+
     for (Short age : ages) {
       if (age < 18) {
         minor++;
@@ -31,9 +39,9 @@ public class HandleGuest {
     System.out.println("a partir de 50: " + senior);
     int total = senior + major + minor;
     if (total != 0) {
-      float perMinor = ((float) minor/ (float) total) * 100;
-      float perMajor = ((float) major/ (float) total) * 100;
-      float perSenior = ((float) senior/ (float) total) * 100;
+      float perMinor = ((float) minor / (float) total) * 100;
+      float perMajor = ((float) major / (float) total) * 100;
+      float perSenior = ((float) senior / (float) total) * 100;
       System.out.println("----- Percentual -----");
       System.out.printf("menores: %.1f", perMinor);
       System.out.println("%");
--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,7 +1,7 @@
 package com.trybe.acc.java.controledeacesso;
 
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Principal {
   /**
@@ -13,11 +13,11 @@ public class Principal {
     ArrayList<Short> ages = new ArrayList<>();
     HandleGuest guest = new HandleGuest();
     do {
-      System.out.println("Entre com o número correspondente à opção desejada:\n" +
-          "1 - Acessar o estabelecimento\n" +
-          "2 - Finalizar sistema e mostar relatório");
+      System.out.println("Entre com o número correspondente à opção desejada:\n"
+          + "1 - Acessar o estabelecimento\n"
+          + "2 - Finalizar sistema e mostar relatório");
       option = Short.parseShort(scanner.next());
-      if(option != 1 && option != 2) {
+      if (option != 1 && option != 2) {
         System.out.println("Entre com uma opção válida!");
       } else if (option != 2) {
         System.out.println("Entre com a idade:");
